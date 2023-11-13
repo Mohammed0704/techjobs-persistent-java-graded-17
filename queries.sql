61d675b1231217060786d533638675babf46a4b6
Mohammed0704 <116527392+Mohammed0704@users.noreply.github.com>
2023-11-13 00:10:10 -0500

    All Tasks Completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
---Part 1
+--Part 1 list the columns and their data types in the table as a SQL comment.
+-- DESCRIBE job;
 
---Part 2
+--Part 2 write a query to list the names of the employers in St. Louis City. Do NOT specify an ordering for the query results.
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
---Part 3
+--Part 3 write the SQL statement to remove the job table.
+DROP TABLE job;
 
---Part 4
+--Part 4 write a query to return the names of all skills that are attached to jobs in alphabetical order. If a skill does not have a job listed, it should not be included in the results of this query.
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,32 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Skill extends AbstractEntity {
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Skill(String description){
+        this.description = description;
+    }
+    public Skill() {};
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
